e623a6c40ada12a20bf50a71dbc25291f86106da
jspark311 <josh.lindsay@gmail.com>
2021-10-31 00:32:57 -0600

    Updating build system.

--- downloadDeps.sh
+++ downloadDeps.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #
 # This script is meant to go fetch the most recent versions of various libraries that
-#   ManuvrOS has been written against. None of this is strictly required for a basic build,
-#   but most real-world applications will want at least one of them.
+#   this program has been written against.
+mkdir -p components
 
-# Manuvr
-#rm -rf lib/ManuvrOS
-git clone https://github.com/Manuvr/ManuvrOS lib/ManuvrOS
+# ManuvrDrivers
+rm -rf components/ManuvrDrivers
+git clone https://github.com/jspark311/ManuvrDrivers.git components/ManuvrDrivers
+
+# CppPotpourri
+rm -rf components/CppPotpourri
+git clone https://github.com/jspark311/CppPotpourri.git components/CppPotpourri
+
+# Teensyduino and support libraries...
+rm -rf components/ManuvrPlatforms
+git clone https://github.com/jspark311/ManuvrPlatforms.git components/ManuvrPlatforms
+ln -s components/ManuvrPlatforms/ESP32 components/ManuvrPlatform
